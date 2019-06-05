c127fae5609b4ca30a1f792272802661617248c8
lokiSpy <lokeshsingh.singh2@gmail.com>
2019-06-05 21:50:31 +0530

    Latest

--- arrays/longest_common_prefix.java
+++ arrays/longest_common_prefix.java
@@ -3,91 +3,68 @@ import java.util.Scanner;
 
 public class longest_common_prefix {
 	
-	public static int sum(int n , int sum)
-	{
-         if( n==0 )
-         {
-        	 return 0 ;
-         }
-         int temp = sum % 10 ;
-         
-         int myans = sum(n/10 , sum ) ;
-         
-         temp += myans ;
-         if(temp<10)
-         {
-        	 sum = temp ;
-         }
-         
-         
-         ///
-         return sum ;
-	}
-
+	
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in) ;
 		
-		int n[] = {9,9,9} ;
-		System.out.println(sum(27 , 0));
+		
+		int T = s.nextInt() ;
+		
+		for(int p = 0 ; p<T ; p++)
+		{
+		int n = s.nextInt() ;
+		
+		String str[] = new String[n] ;
+		
+		for(int x = 0 ; x<n ; x++)
+		{
+			str[x] = s.next() ;
+		}
+		
+		if(n <=1)
+		{
+			for(int x = 0 ; x<n ; x++)
+			{
+				System.out.println(str[x]);
+			}
+			return ;
+		}
+		
+		//int count = 0 ;
+		String ans = "" ;
 		
 		
-//		int T = s.nextInt() ;
-//		
-//		for(int p = 0 ; p<T ; p++)
-//		{
-//		int n = s.nextInt() ;
-//		
-//		String str[] = new String[n] ;
-//		
-//		for(int x = 0 ; x<n ; x++)
-//		{
-//			str[x] = s.next() ;
-//		}
-//		
-//		if(n <=1)
-//		{
-//			for(int x = 0 ; x<n ; x++)
-//			{
-//				System.out.println(str[x]);
-//			}
-//			return ;
-//		}
-//		
-//		//int count = 0 ;
-//		String ans = "" ;
-//		
-//		
-//		for(int i=0 ; i<str.length-1 ; i++)
-//		{
-//			String tempans = "" ;
-//			//int temp = 0 ;
-//			for(int j = 0 ; j<str[i].length() && j<str[i+1].length(); j++)
-//			{
-//				if( str[i].charAt(j) == str[i+1].charAt(j) )
-//				{
-//					//count ++ ;
-//					tempans += str[i].charAt(j) ;
-//					//temp ++ ;
-//				}
-//				else
-//				{
-//					break ;
-//				}
-//			}
-//			//System.out.println(ans.length());
-//			if(i==0)
-//				ans = tempans ;
-//			else if(tempans.length()<ans.length())
-//				ans = tempans ;
-//		}
-//		if(ans.length() == 0)
-//			System.out.println(-1);
-//		else
-//			System.out.println(ans);
-//		
-//		
-//		}
+		for(int i=0 ; i<str.length-1 ; i++)
+		{
+			String tempans = "" ;
+			//int temp = 0 ;
+			for(int j = 0 ; j<str[i].length() && j<str[i+1].length(); j++)
+			{
+				if( str[i].charAt(j) == str[i+1].charAt(j) )
+				{
+					//count ++ ;
+					tempans += str[i].charAt(j) ;
+					//temp ++ ;
+				}
+				else
+				{
+					break ;
+				}
+			}
+			//System.out.println(ans.length());
+			if(i==0)
+				ans = tempans ;
+			else if(tempans.length()<ans.length())
+				ans = tempans ;
+		}
+		if(ans.length() == 0)
+			System.out.println(-1);
+		else
+			System.out.println(ans);
+		
+		
+		}
 	}
 
 }
--- arrays/specialsumofarray.java
+++ arrays/specialsumofarray.java
@@ -2,9 +2,33 @@ import java.util.*;
 
 public class specialsumofarray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static int sum(int n , int s)
+	{
+         if( n==0 )
+         {
+        	 return 0 ;
+         }
+         int temp = n % 10 ;
+         
+         int myans = sum(n/10 , s ) ;
+         
+         temp += myans ;
+         if(temp<10)
+         {
+        	 s = temp ;
+         }
+         
+         
+         ///
+         return s ;
+	}
 
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in) ;
+		
+		int n[] = {9,9,9} ;
+		System.out.println(sum(32 , 0));
 	}
 
 }
